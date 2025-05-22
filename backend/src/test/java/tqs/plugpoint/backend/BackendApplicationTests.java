d7e1780b359e53493a7208ab342a693ee79419f2
Hugo Sousa <sousahugo@ua.pt>
2025-05-22 17:37:02 +0100

    potential fix

--- backend/src/test/java/tqs/plugpoint/backend/BackendApplicationTests.java
+++ backend/src/test/java/tqs/plugpoint/backend/BackendApplicationTests.java
@@ -2,7 +2,9 @@ package tqs.plugpoint.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")	
 @SpringBootTest
 class BackendApplicationTests {
 
